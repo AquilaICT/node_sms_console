0184c208d5bf4181e32a45cc950510d86d6858da
AmanuelDamtachew <34443065+AmanuelDamtachew@users.noreply.github.com>
2023-02-20 10:02:32 +0300

    IRC_feb_20_2023_KONSO_IRC

--- node_sms_console/config.js
+++ node_sms_console/config.js
@@ -9,7 +9,7 @@ export default {
   JASMINE_RESTFULL_API: 'http://127.0.0.1:8080/secure',
   GEEZ_RESTFULL_API: 'https://api.geezsms.com/api',
   STORAGE_RESTFULL_API: 'http://localhost:8002/api',
-  job_name: "A2pBULK_KONSO_IRC-2023-02-20",
+  job_name: "A2pBULK_KONSO_IRC_2023-02-20",
   group_id: 'KONSO_IRC',
   progress: 0,
   msg: "Ajaylaa \nPirokiraameta raatoneta ahellaa shashakka Isho a kollissampateeta aafaa Xonso \nToollaaye Ihhalaatin \nAwta  Piisa Lankayya Etaa teykanta 3:40-4:00, Palawwa kalpta Etaa12:20-12:40 Arpaminji \nEffi Emmeeta 90.9 Olliyyi. \nAkka ishin kapannin tarpenin \nHellaa poraa oppan kahadan ka kolladan  iharmisamte",
